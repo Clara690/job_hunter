c20ea738763b6264d658da279cff6c410d5144ec
clara <clara80416@gmail.com>
2026-06-08 12:12:05 +0200

    Added docker file

--- scraper/producer_multi_queue.py
+++ scraper/producer_multi_queue.py
@@ -3,10 +3,21 @@ from scraper.tasks_cake_scraper import scrape_cake_jobs
 
 
 # ------ 104 ------ #
-task_104 = scrape_104_jobs.s("資料工程師", 1)
+
+# the roles to look for and the page number to scrape
+search_keyword, page = "資料工程師", 1
+
+# send the task to the "104_jobs" queue
+task_104 = scrape_104_jobs.s(search_keyword, page)
 task_104.apply_async(queue="104_jobs") # send the task to the "104_jobs" queue 
 print("Task for 104 scraper has been sent to the queue.")
+
 # ------ Cake ------ #
-task_cake = scrape_cake_jobs.s("data engineer")
+
+# the roles to look for 
+search_keyword = "data engineer"
+
+# send the task to the "cake_jobs" queue
+task_cake = scrape_cake_jobs.s(search_keyword)
 task_cake.apply_async(queue="cake_jobs") # send the task to the "cake_jobs" queue
 print("Task for Cake scraper has been sent to the queue.")
