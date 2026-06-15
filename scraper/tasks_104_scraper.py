db8d7d1bf14dfff3b04f1ab6a79848b34ce962ef
clara <clara80416@gmail.com>
2026-06-15 13:34:18 +0200

    Modified meta data of the table

--- scraper/tasks_104_scraper.py
+++ scraper/tasks_104_scraper.py
@@ -7,6 +7,7 @@ from loguru import logger
 from sqlalchemy import create_engine
 from sqlalchemy.dialects.mysql import insert
 from sqlalchemy.pool import NullPool
+from sqlalchemy.exc import OperationalError
 from sqlalchemy import MetaData, Table, Column, Integer, String, CHAR, Text, TIMESTAMP, UniqueConstraint, text
 from scraper.config import MYSQL_ACCOUNT, MYSQL_HOST, MYSQL_PASSWORD, MYSQL_PORT
         
@@ -22,7 +23,7 @@ jobs_table = Table(
     "jobs_104", 
     metadata,
     Column("id", Integer, primary_key=True, autoincrement=True),
-    Column("job_name", String(100), nullable=False),
+    Column("job_name", String(255), nullable=False),
     Column("company", String(50), nullable=False),
     Column("location", String(50), nullable=False),
     Column("experience", Integer, nullable=False),
@@ -87,59 +88,11 @@ def scrape_104_jobs(search_term, page):
         print(f"An error occurred: {e}")
         return None
 
-
-
-# for printing out the results in the console
-# the task is basically the same as scrape_104_jobs
-@app.task()
-def scrape_104_jobs_print(search_term, page):
-    based_url = "https://www.104.com.tw/jobs/search/api/jobs"
-
-    # define the parameters for the GET request
-    params = {
-        "asc": 1,
-        "jobsource": "joblist_search",
-        "keyword": search_term,
-        "mode": "s",
-        "order": 4,
-        "page": page,  # Inject the current page number
-        "pagesize": 20,
-        "searchJobs": 1,
-    }
-    headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/148.0.0.0 Safari/537.36",
-        "Referer": "https://www.104.com.tw/jobs/search/",
-    }
-    # for storing the results
-    jobs = []
-    # the acutal scraping process
-    try:
-        response = requests.get(based_url, params=params, headers=headers)
-        if response.status_code == 200:
-            data = response.json()
-            data = data["data"]
-            for job in data:
-                description = {
-                    "job_name": job["jobName"],
-                    "company": job["custName"],
-                    "location": job["jobAddrNoDesc"],
-                    "experience": job["jobRo"],
-                    "remote": job["remoteWorkType"],
-                    "salary_low": job["salaryLow"],
-                    "salary_high": job["salaryHigh"],
-                    "link": job["link"]["job"],
-                }
-                jobs.append(description)
-            print(jobs)
-        else:
-            print(f"Failed to retrieve data: {response.status_code}")
-            return None
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return None
     
 # upload to MySQL in one task
-@app.task(bind=True, max_retries=3)
+@app.task(bind=True, autoretry_for=(OperationalError,), 
+    retry_backoff=True, # waits between retries,
+    max_retries=3)
 def scrape_104_jobs_upload_mysql(self, search_term, page):
     
     # the data scraped from the website
