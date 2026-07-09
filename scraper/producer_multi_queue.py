499a32d5a232357f118c520a34d94eab404f1e13
clara <clara80416@gmail.com>
2026-07-09 16:09:01 +0200

    Added a new task

--- scraper/producer_multi_queue.py
+++ scraper/producer_multi_queue.py
@@ -1,8 +1,9 @@
 from scraper.tasks_104_scraper import scrape_104_jobs_upload_mysql
 from scraper.tasks_cake_scraper import scrape_cake_jobs_upload_mysql
+from scraper.tasks_get_exchange_rate import refresh_exchange_rates
+from loguru import logger
 
-
-# ------ 104 ------ loop ver. #
+# ------ 104 ------ #
 # start page and end page
 start = 1
 end = 35
@@ -17,9 +18,9 @@ for search_term in search_terms:
 
 
 
-# ------ Cake ------ #
+# # ------ Cake ------ #
 
-# the roles to look for on cake
+# # the roles to look for on cake
 search_keywords = ['data engineer', 'data analyst', 'ml engineer', 
                    'software engineer', 'software developer', 'database engineer']
 start = 1
@@ -29,4 +30,10 @@ for search_keyword in search_keywords:
     # send the task to the 'cake_jobs' queue
         task_cake = scrape_cake_jobs_upload_mysql.s(search_keyword, page_num)
         task_cake.apply_async(queue='cake_jobs') # send the task to the 'cake_jobs' queue
-    print(f'{search_keyword} a total of:{end - start +1} tasks task for Cake scraper has been sent to the queue.')
+        logger.info(f'{search_keyword} a total of:{end - start +1} tasks task for Cake scraper has been sent to the queue.')
+
+
+# ------ fetch exchange rate ------ #
+task_ex_rate = refresh_exchange_rates.s()
+task_ex_rate.apply_async(queue='exchange') 
+logger.info(f'Fetching exhcnage rate and uploading to database...')
